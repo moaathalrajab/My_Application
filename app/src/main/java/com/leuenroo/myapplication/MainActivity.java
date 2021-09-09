685fd3eb0b599709dceba50013b9d55875f60e6e
Moaath Alrajab <moaath@alrajab.net>
2021-09-09 15:45:44 -0400

    Add btn key

--- app/src/main/java/com/leuenroo/myapplication/MainActivity.java
+++ app/src/main/java/com/leuenroo/myapplication/MainActivity.java
@@ -3,12 +3,24 @@ package com.leuenroo.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.txt);
+
+
+    }
+
+    public void chnageText(View view) {
+
+        tv1.setText("Hello Me Moaath Here ");
     }
 }
